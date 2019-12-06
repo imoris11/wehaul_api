31d25c1e94277661fb7fe1ad8bb64d82e78e5e30
Richard Igbiriki <richardigbiriki@Richards-MacBook-Pro-2.local>
2019-12-06 17:46:24 +0100

    delete factories of unwanted files

--- spec/factories/categories.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-    factory :category do
-        title {Faker::Lorem.word}
-        token {nil}
-    end
-end
--- spec/factories/schedules.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-    factory :schedule do
-        title {Faker::Lorem.sentence}
-        date_time {Time.now}
-        header_photo {Faker::Avatar.image}
-        user_id {nil}
-    end
-end
