1c9fc20162613b588c88d9ef2c13a29f6e0e41aa
Richard Igbiriki <richardigbiriki@Richards-MacBook-Pro-2.local>
2019-12-30 23:43:53 +0100

    add created at to serializer

--- app/serializers/trip_activity_serializer.rb
+++ app/serializers/trip_activity_serializer.rb
@@ -1,4 +1,4 @@
 class TripActivitySerializer < ActiveModel::Serializer
-  attributes :id, :activity, :mark_as_responded, :token
+  attributes :id, :activity, :mark_as_responded, :created_at, :token
   has_one :trip_request
 end
--- app/serializers/trip_request_serializer.rb
+++ app/serializers/trip_request_serializer.rb
@@ -1,5 +1,5 @@
 class TripRequestSerializer < ActiveModel::Serializer
-  attributes :id, :driver_fee, :commission, :distance, :is_approved_admin, :is_approved_user, :is_paid, :driver_paid, :trip_amount, :contact_address, :pickup_time, :fee, :pickup_date, :quantity, :token, :driver_id, :status, :weight, :contact_number, :status, :loader, :contact_name, :contact_email, :destination_address, :pickup_address, :vehicles_qty, :description, :created_by
+  attributes :id, :driver_fee, :commission, :distance, :is_approved_admin, :is_approved_user, :is_paid, :driver_paid, :trip_amount, :contact_address, :pickup_time, :fee, :pickup_date, :quantity, :token, :driver_id, :status, :weight, :contact_number, :status, :loader, :contact_name, :contact_email, :destination_address, :pickup_address, :vehicles_qty, :description, :created_by, :created_at
   has_one :user
   has_one :vehicle_type
 end
